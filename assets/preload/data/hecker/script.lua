85eb61b121d5de33a2dbe6c5836ad9dd90e04c5d
Danibomfin <98532797+Danibomfin@users.noreply.github.com>
2022-08-05 12:37:07 -0300

    Add files via upload

---
+++ assets/preload/data/hecker/script.lua
@@ -0,0 +1,16 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bfded'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'death'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'silence'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'deathconfirm'); --put in mods/music/	
+end
+
+local allowCountdown = false
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
+		startVideo('cutscene1');
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
