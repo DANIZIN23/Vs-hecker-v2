f1ac4914bba2a1c8514a84efaf610e6b61621561
Danibomfin <98532797+Danibomfin@users.noreply.github.com>
2022-08-05 12:39:55 -0300

    Add files via upload

---
+++ assets/preload/data/think/script.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bfdedcall'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'death'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'silence'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'deathconfirm'); --put in mods/music/	
+end
